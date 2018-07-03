5e2dbb39fb4aa28e57a7b15146056d41b7c90dd8
middleweight <bam@middleweight.co>
2018-07-03 22:11:05 +0800

    Extras
    Turnery operator

--- src/index.js
+++ src/index.js
@@ -6,23 +6,38 @@ import "./styles.css";
 var magician = {
   name: "Hoodini",
   skill: "Escape",
-  location: ""
+  location: "",
+  avatar: "dini.png"
 };
 
+var assistant = {
+  name: "Jessie",
+  skill: "Dance",
+  location: "Liverpool",
+  avatar: "girl.png"
+}
+
 function getLocation(location) {
-if(location){
+if(location) {
   return location
 } else {
   return "Mystery"
 }
 }
 
+function magicianPic (avatar) {
+ if (avatar) {
+   return <p> Picture: {avatar} </p>
+ }
+}
+// turnery operator
 function App() {
   return (
     <div className="App">
-      <h1>{magician.name}</h1>
+      <h1>{magician.name ? magician.name : "Name hidden"}</h1>
       <h2>{magician.skill}</h2>
       <h3>{getLocation(magician.location)} </h3>
+      {magicianPic(magician.avatar)}
     </div>
   );
 }
