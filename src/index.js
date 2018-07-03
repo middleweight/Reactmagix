126953fafb758a1397f31c3e52e34d89acb3d9cf
middleweight <bam@middleweight.co>
2018-07-03 21:36:20 +0800

    Start conditions
    Add some conditioning

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import "./styles.css";
 
 var magician = {
   name: "Hoodini",
-  skill: "Escape"
+  skill: "Escape",
+  location: ""
 };
+
+function getLocation(location) {
+if(location){
+  return location
+} else {
+  return "Mystery"
+}
+}
 
 function App() {
   return (
     <div className="App">
       <h1>{magician.name}</h1>
       <h2>{magician.skill}</h2>
+      <h3>{getLocation(magician.location)} </h3>
     </div>
   );
 }
